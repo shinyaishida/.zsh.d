683f36264e2c5e70e9c8fa58483b232c15a6475a
Shinya Ishida <zhinzai@gmail.com>
2017-08-16 17:28:09 +0900

    Modified a code error.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pwd=${0%%setup.sh}
+pwd=$(pwd)/${0%%setup.sh}
 zshd="$HOME"/.zsh.d
 files='profile shenv shrc'
 
@@ -8,14 +8,14 @@ cd $HOME
 if [ -e ${zshd} ]; then
     echo "${zshd} exists. Aborted."
 else
-    ln -s ${pwd} .zsh.d
+    echo ln -s ${pwd} .zsh.d
     for f in ${files}; do
 	file=".z$f"
 	if [ -e ${file} ]; then
 	    echo "$HOME/$file exists. Aborted."
 	    break
 	else
-	    ln -s ${pwd}z$f $file
+	    echo ln -s ${pwd}z$f $file
 	fi
     done
 fi
