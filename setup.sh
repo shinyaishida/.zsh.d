108ffa491f8c1e3e72f51875ebf1b65d4e65272b
Shinya Ishida <91529415+shinya-asterisk@users.noreply.github.com>
2022-12-30 11:02:29 +0900

    shellcheck

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 readonly zshd=.zsh.d
-readonly zsh_home=$HOME/${zshd}
+readonly zsh_home="$HOME/${zshd}"
 readonly files='zprofile zshenv zshrc'
 
-[ ! -e ${zsh_home} ] && {
-  ln -s $(cd ${0%/*}; pwd) ${zsh_home}
-}
+if [ ! -d "${zsh_home}" ]; then
+  cd "${0%/*}" || exit 1
+  ln -s "$(pwd)" "${zsh_home}"
+  cd - || exit 2
+fi
 for file in ${files}; do
-  dotfile=$HOME/.${file}
-  [ -e ${dotfile} ] && \
-    echo "${dotfile} exists." || \
-    ln -s ${zsh_home}/${file} ${dotfile}
+  dotfile="$HOME/.${file}"
+  if [ -e "${dotfile}" ]; then
+    echo "${dotfile} exists."
+  else
+    ln -s "${zsh_home}/${file}" "${dotfile}"
+  fi
 done
